b4d926b5db6b57c91393779232c02c6797ef9f93
snufka <52801250+snufka@users.noreply.github.com>
2019-11-29 22:42:11 +0100

    commit

---
+++ app.js
@@ -0,0 +1,31 @@
+class Employee {
+    constructor(this) {
+        this.name = name;
+        this.id = id;
+    }
+    getId() {
+        return this.id
+    };
+    getEmail() {
+
+    };
+    getRole() { return "Employee" };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
