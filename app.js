72cfe6422c79f40977e8dac538b237a186f1d630
snufka <52801250+snufka@users.noreply.github.com>
2019-12-07 23:00:22 +0100

    commit

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
-class Employee {
-    constructor() {
-        this.name = name;
-        this.id = id;
-        this.title = title;
+const questions = [
+    {
+        name: "name",
+        message: "Please enter a Team Member Name?"
+    },
+    {
+        name: "id",
+        message: "What is the Team Memeber´s Id?"
+    },
+    {
+        name: "mail",
+        message: "What is the Team Memeber´s Email?"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is the Team Memeber´s Role in the company?",
+        choices: ["Intern", "Engineer"]
+    },
+    {
+        name: "school",
+        message: "In which School does the Team Member study?",
+        when: function (answers) {
+            return answers.role == "Intern";
+        }
+    },
+    {
+        name: "github",
+        message: "What is the Team Memeber´s GitHub Username?",
+        when: function (answers) {
+            return answers.role == "Engineer";
+        }
+    },
+    {
+        type: "confirm",
+        name: "anotherMember",
+        message: "Do you want to add another Team Member?",
+        default: true
     }
-    getName() {
-        return name
-    }
-    getId() {
-        return id
-    };
-    getEmail() {
-
-    };
-    getRole() { return "Employee" };
-}
-
-module.exports = Employee
-
+];
 
 
 
